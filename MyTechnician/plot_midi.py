1b57a37144e367edc581aae65a86595054984b6c
Davide Del Vento <davide.del.vento@gmail>
2022-01-23 15:28:25 -0700

    plotting ADC hammer stuff

--- MyTechnician/plot_midi.py
+++ MyTechnician/plot_midi.py
@@ -2,12 +2,29 @@
 
 import argparse
 from mido import MidiFile
+import matplotlib.pyplot as plt
+from cdefine import CDefine
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file")
 args = parser.parse_args()
 
-print('content of file', args.file, '(no plot yet)')
+defined = CDefine('../RaspberryPiPico/My_MIDI_constants.h')
+
+x = []
+y = []
+i = 0
 
 for msg in MidiFile(args.file).play():
-        print(msg)
+    if msg.type is 'sysex':
+        if (msg.data[0] == defined.MIDI_VENDOR and
+            msg.data[1] <= defined.MIDI_MAX_ADC_VALUE):
+
+            x.append(i)
+            i = i + 1
+            y.append(msg.data[2] + msg.data[1] * 128)
+
+fig, ax = plt.subplots()
+ax.plot(x,y)
+ax.set_ylim(0, 4096);
+plt.show()
