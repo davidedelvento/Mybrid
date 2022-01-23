4aacd965d734659d108e52e12c8d5522d8d2fa6e
Davide Del Vento <davide.del.vento@gmail>
2022-01-23 16:39:04 -0700

    avoid possible conflict with capture of the data in a separate shell

--- MyTechnician/mytechnician/__init__.py
+++ MyTechnician/mytechnician/__init__.py
@@ -8,10 +8,10 @@ defined = CDefine('../RaspberryPiPico/My_MIDI_constants.h')
 pico_in  = mido.get_input_names()[1]
 pico_out = mido.get_output_names()[1]
 
-inport = mido.open_input(pico_in)
+#inport = mido.open_input(pico_in)
 outport = mido.open_output(pico_out)
 
-print("Opened", pico_in, "for input")
+#print("Opened", pico_in, "for input")
 print("Opened", pico_out, "for output")
 
 def adc_dump(note):
