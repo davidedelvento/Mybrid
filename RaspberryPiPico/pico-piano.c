15f54c3b44a4056e553b04b69411aa1beb522d63
Davide Del Vento <davide.del.vento@gmail>
2022-01-23 14:47:58 -0700

    stopping ADC-dumping for non-controllers, changed MIDI constants

--- RaspberryPiPico/pico-piano.c
+++ RaspberryPiPico/pico-piano.c
@@ -45,19 +45,19 @@
 #endif
 
 #define MIDI_SYS_EX              0xF0   // System Exclusive messages
-#define MIDI_VENDOR              0x7D   // Prototype
-#define MIDI_DUMP_NOTE_ADC       0x7C   // custom command to start sending ADC data over MIDI
-#define MIDI_STOP_DUMP_ADC       0x3C   // custom command to stop  sending ADC data over MIDI
-#define MIDI_REGULATE            0x4E   // custom command to start regulating one note
-#define MIDI_CONTINUE_REGULATION 0x1C   // custom command to continue (or finish) regulating
-#define MIDI_DUMP_REGULATION     0x0D   // custom command to dump regulation parameters for one note
 #define MIDI_END_SYSEX           0xF7   // End of System Exclusive, aka EOX
+#define MIDI_VENDOR              0x7D   // Prototype
 
-#define INIT_PICO                0x2D   // custom command to init each pico (observed only at the beginning)
-
-#define MIDI_ITER_PER_MS         0x40   // bits the ADC will never use, adding one for each pico, can be max 0x7C
-#define MIDI_NO_SUCH_NOTE        0x7D   // bits the ADC will never use
-#define MIDI_ERROR               0x7E   // bits the ADC will never use
+#define MIDI_DUMP_NOTE_ADC       0x7F   // custom command to start sending ADC data over MIDI
+#define MIDI_STOP_DUMP_ADC       0x7E   // custom command to stop  sending ADC data over MIDI
+// we could probably use 0x7D skipping to avoid confusion with MIDI_VENDOR
+#define MIDI_REGULATE            0x7C   // custom command to start regulating one note
+#define MIDI_CONTINUE_REGULATION 0x7B   // custom command to continue (or finish) regulating
+#define MIDI_DUMP_REGULATION     0x7A   // custom command to dump regulation parameters for one note
+#define INIT_PICO                0x79   // custom command to init each pico (observed only at the beginning)
+#define MIDI_NO_SUCH_NOTE        0x78   // bits the ADC will never use
+#define MIDI_ERROR               0x77   // bits the ADC will never use
+#define MIDI_ITER_PER_MS         0x40   // bits the ADC will never use, adding one for each pico, can be max 0x77
 
 #define MIDI_NOTE_ON       0x90
 #define MIDI_NOTE_OFF      0x80
@@ -375,11 +375,11 @@ void dump_note_adc(uint8_t my_note) {
   while(true) {
 #ifdef MIDI_CONTROLLER
     tud_task(); // keep MIDI over USB alive
-#endif
     if (tud_midi_available()) {
-      // TODO needs to read over i2c for the other picos
-      // outside to the tud_midi_available if...
       tud_midi_stream_read(packet, SYSEX_PKG_LEN);
+#elif defined WORKER
+    if (queue_try_remove(&sysex_buffer, packet)) {
+#endif
       if (packet[0] == MIDI_SYS_EX && packet[1] == MIDI_VENDOR) {
         if (packet[2] == MIDI_STOP_DUMP_ADC) {
 	  break;
