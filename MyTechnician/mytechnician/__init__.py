a710d8cc021d9c6ae16f9a0a4d0e46280c64c72c
Davide Del Vento <davide.del.vento@gmail>
2022-01-29 14:14:00 -0700

    multithreaded interactive technician

--- MyTechnician/mytechnician/__init__.py
+++ MyTechnician/mytechnician/__init__.py
@@ -4,15 +4,71 @@ import mido
 from mido import Message, MidiFile, MidiTrack
 from cdefine import CDefine
 
-defined = CDefine('../RaspberryPiPico/My_MIDI_constants.h')
+import threading, time, blessed
+
 pico_in  = mido.get_input_names()[1]
 pico_out = mido.get_output_names()[1]
+defined = CDefine('../RaspberryPiPico/My_MIDI_constants.h')
 
-#inport = mido.open_input(pico_in)
-outport = mido.open_output(pico_out)
+class mt:
+    def __init__(self):
+        self.th = None
+        self.t = blessed.Terminal()
+        self.outport = mido.open_output(pico_out)
+        print("Opened", pico_out, "for output")
+        self.must_stop = True
+        self.mid = None
+        self.track = None
 
-#print("Opened", pico_in, "for input")
-print("Opened", pico_out, "for output")
+    def _print_above(self, stuff):
+        with self.t.location(self.t.width - len(stuff) - 1, 0):
+            print(stuff, end=None)
+
+    def _capture(self, pico):
+        with mido.open_input(pico) as inport:
+            print(pico, "opened, collecting messages.")
+            print("Run `save_captured(file)` to stop capturing and save.")
+            print("Run `abort_capture()` to stop capturing.")
+            last_time = 0
+            options = ["|", "/", "-", "\\"]
+            i = 0
+            self._print_above(options[i])
+            for msg in inport:
+                self.track.append(msg)
+                curr_time = time.time()
+                if (curr_time - last_time > .25):
+                    i = (i+1) % len(options)
+                    self._print_above(options[i])    # TODO print MIDI housekeeping
+                    curr_time = last_time
+                if self.must_stop:
+                    break
+        print("Capture stopped")
+
+    def abort_capture(self):
+        self.must_stop = True
+        print("Waiting for last packet to quit")
+        self.th.join()
+
+    def capture(self, pico=pico_in):
+        self.mid = MidiFile()
+        self.must_stop = False
+
+        self.track = MidiTrack()
+        self.mid.tracks.append(self.track)
+        self.th = threading.Thread(target = mt._capture, args = (self, pico) )
+        self.th.start()
+        time.sleep(1) # let the _print_above win the race condition agains the prompt
+
+
+    def save_captured(self, filename):
+        if type(filename) != str:
+            raise ValueError("first argument must be a string")
+
+        self.must_stop = True
+        self.mid.save(filename)
+        print("File saved, waiting for last packet")
+        self.th.join()
+
 
 def adc_dump(note):
     outport.send(mido.Message('sysex', data=(
