005a6e4d0e06b47a78a6e5961b6c0f0ced8d2610
Davide Del Vento <davide.del.vento@gmail>
2022-02-05 12:12:40 -0700

    more sensible arguments

--- MyTechnician/parse.py
+++ MyTechnician/parse.py
@@ -2,17 +2,19 @@
 
 import argparse, mido, bz2
 from mido import Message, MidiFile, MidiTrack
-from cdefine import CDefine
 import mytechnician
 
 parser = argparse.ArgumentParser()
-parser.add_argument("filename", help="Load <FILENAME> for immediate plotting or text file dumping")
-parser.add_argument("--dump", help="Dump the content of FILENAME on the terminal", action="store_true")
-parser.add_argument("-i", "--ignore-midi-time", help="Use the ADC values sequentially, disregarding MIDI time packets", action="store_true")
-parser.add_argument("-s", "--silent", help="Do not report housekeeping messages", action="store_true")
+parser.add_argument("filename", help="Load <FILENAME> for plotting, analysis or dumping in a text file")
+action = parser.add_mutually_exclusive_group(required=True)
+action.add_argument("-d", "--dump", help="Dump the content of FILENAME on the terminal", action="store_true")
+action.add_argument("-p", "--plot", help="Plot the content of FILENAME with matplotlib", action="store_true")
+action.add_argument("-s", "--stat", help="Print summary statistics about FILENAME", action="store_true")
+action.add_argument("-a", "--analysis", help="Plot analysis for MIDI velocity from ADC dump", action="store_true")
+parser.add_argument("--ignore-midi-time", help="Use the ADC values sequentially, disregarding MIDI time", action="store_true")
+parser.add_argument("-q", "--quiet", help="Do not report housekeeping messages", action="store_true")
 args = parser.parse_args()
 
-defined = CDefine('../RaspberryPiPico/My_MIDI_constants.h')
 mt = mytechnician.mt()
 
 def load_data():
